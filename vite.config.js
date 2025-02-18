72255b7aedc04799c8eaf56d02ff588fbe6151c6
Kashyap-Bhvajjula <bh.kashyap.b@gmail.com>
2025-02-18 18:40:01 +0530

    Proxy added2

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://user-management-api-backend.onrender.com/users', // Your backend server URL
-        changeOrigin: true, // For virtual hosted sites
-        secure: false, // If your backend uses HTTP
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optionally rewrite paths
-      },
-    },
-  },
 })
